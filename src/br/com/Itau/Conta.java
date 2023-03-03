dc9f443e5b4d258bd3e7afa18add0cce23fdbd4d
Fellpz <108369176+gitFellpz@users.noreply.github.com>
2023-03-03 15:47:16 -0300

    Criando atributos.

--- src/br/com/Itau/Conta.java
+++ src/br/com/Itau/Conta.java
@@ -4,4 +4,11 @@ import java.util.Scanner;
 
 public class Conta {
 	Scanner input = new Scanner(System.in);
+	
+	//Atributos
+	public int numConta;
+	protected int tipoConta;
+	private String donoConta;
+	private double saldo;
+	private boolean status;
 }
